b8f411da0d1524c02e4161b394e2f5a177048128
tim <ctom3560@gmail.com>
2025-10-02 23:29:35 +0800

    add generic linked list

--- week3/linked_list/src/linked_list.rs
+++ week3/linked_list/src/linked_list.rs
@@ -1,24 +1,24 @@
 use std::fmt;
 use std::option::Option;
 
-pub struct LinkedList {
-    head: Option<Box<Node>>,
+pub struct LinkedList<T> {
+    head: Option<Box<Node<T>>>,
     size: usize,
 }
 
-struct Node {
-    value: u32,
-    next: Option<Box<Node>>,
+struct Node<T> {
+    value: T,
+    next: Option<Box<Node<T>>>,
 }
 
-impl Node {
-    pub fn new(value: u32, next: Option<Box<Node>>) -> Node {
+impl<T> Node<T> {
+    pub fn new(value: T, next: Option<Box<Node<T>>>) -> Node<T> {
         Node {value: value, next: next}
     }
 }
 
-impl LinkedList {
-    pub fn new() -> LinkedList {
+impl<T> LinkedList<T> {
+    pub fn new() -> LinkedList<T> {
         LinkedList {head: None, size: 0}
     }
     
@@ -30,24 +30,25 @@ impl LinkedList {
         self.get_size() == 0
     }
     
-    pub fn push_front(&mut self, value: u32) {
-        let new_node: Box<Node> = Box::new(Node::new(value, self.head.take()));
+    pub fn push_front(&mut self, value: T) {
+        let new_node: Box<Node<T>> = Box::new(Node::new(value, self.head.take()));
         self.head = Some(new_node);
         self.size += 1;
     }
     
-    pub fn pop_front(&mut self) -> Option<u32> {
-        let node: Box<Node> = self.head.take()?;
-        self.head = node.next;
+    pub fn pop_front(&mut self) -> Option<T> {
+        let node: Box<Node<T>> = self.head.take()?;
+        self.head = node.next; // move next node to head; the old head is dropped automatically
         self.size -= 1;
         Some(node.value)
     }
 }
 
 
-impl fmt::Display for LinkedList {
+// trait bound to ensure T is displayable
+impl<T: std::fmt::Display> fmt::Display for LinkedList<T> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let mut current: &Option<Box<Node>> = &self.head;
+        let mut current: &Option<Box<Node<T>>> = &self.head;
         let mut result = String::new();
         loop {
             match current {
@@ -62,7 +63,7 @@ impl fmt::Display for LinkedList {
     }
 }
 
-impl Drop for LinkedList {
+impl<T> Drop for LinkedList<T> {
     fn drop(&mut self) {
         let mut current = self.head.take();
         while let Some(mut node) = current {
@@ -72,4 +73,45 @@ impl Drop for LinkedList {
 }
 
 
+// trait bound to ensure T is copyable
+impl<T: Copy> Clone for LinkedList<T> {
+    fn clone(&self) -> Self {
+        let mut new_list = LinkedList::new();
+        let mut current = &self.head;
+        let mut vec = Vec::new();
+        while let Some(node) = current {
+            current = &node.next;
+            vec.push(node);
+        }
+        // push in reverse order to maintain the same order in the new list
+        for node in vec.iter().rev() {
+            new_list.push_front(node.value);
+        } 
+        new_list
+    }
+}
 
+// trait bound to ensure T is comparable
+impl<T: std::cmp::PartialEq> PartialEq for LinkedList<T> {
+    fn eq(&self, other: &Self) -> bool {
+        if self.size != other.size {
+            return false;
+        }
+        let mut self_current = &self.head;
+        let mut other_current = &other.head;
+        for _ in 0..self.size {
+            if let (Some(self_node), Some(other_node)) = (self_current, other_current) {
+                if self_node.value != other_node.value {
+                    return false;
+                }
+            }
+            if let Some(node) = self_current {
+                self_current = &node.next;
+            }
+            if let Some(node) = other_current {
+                other_current = &node.next;
+            }
+        }
+        true
+    }
+}
--- week3/linked_list/src/main.rs
+++ week3/linked_list/src/main.rs
@@ -15,6 +15,27 @@ fn main() {
     println!("size: {}", list.get_size());
     println!("{}", list.to_string()); // ToString impl for anything impl Display
 
+
+    let mut list2: LinkedList<String> = LinkedList::new();
+    assert!(list2.is_empty());
+    assert_eq!(list2.get_size(), 0);
+    for i in 1..12 {
+        list2.push_front(i.to_string());
+    }
+    println!("{}", list2);
+    println!("list2 size: {}", list.get_size());
+    println!("top element: {}", list2.pop_front().unwrap());
+    println!("{}", list2);
+    println!("size: {}", list2.get_size());
+    println!("{}", list2.to_string()); // ToString impl for anything impl Display
+
+    // Test Clone and PartialEq
+    let mut list_clone: LinkedList<u32> = list.clone();
+    assert_eq!(list.get_size(), list_clone.get_size());
+    assert!(list == list_clone);
+    list_clone.pop_front();
+    assert!(list != list_clone);
+   
     // If you implement iterator trait:
     //for val in &list {
     //    println!("{}", val);

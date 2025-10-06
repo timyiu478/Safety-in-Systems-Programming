591281c1fdc697bf226ddbd7f77f5b7c01bb9b0a
tim <ctom3560@gmail.com>
2025-10-06 19:04:14 +0800

    add week6

---
+++ week6/README.md
@@ -0,0 +1,30 @@
+# Implementation idae 
+
+Constraints:
+
+1. only use channel as synchronization mechanism
+1. respect the order of inputs
+
+![](assets/idea.png)
+
+# Example Run
+
+```bash
+tim@tim-virtual-machine ~/g/S/w/parallel_map (main)> cargo run samples/sleepy_print
+6 squared is 36
+10 squared is 100
+4 squared is 16
+8 squared is 64
+7 squared is 49
+9 squared is 81
+1 squared is 1
+5 squared is 25
+2 squared is 4
+3 squared is 9
+18 squared is 324
+5 squared is 25
+11 squared is 121
+20 squared is 400
+12 squared is 144
+squares: [36, 49, 64, 81, 100, 1, 4, 9, 16, 25, 144, 324, 121, 25, 400]
+```
--- week6/parallel_map/src/main.rs
+++ week6/parallel_map/src/main.rs
@@ -8,7 +8,39 @@ where
     U: Send + 'static + Default,
 {
     let mut output_vec: Vec<U> = Vec::with_capacity(input_vec.len());
-    // TODO: implement parallel map!
+    output_vec.resize_with(input_vec.len(), Default::default);
+
+    let (in_sender, in_receiver) = crossbeam_channel::unbounded();
+    let (out_sender, out_receiver) = crossbeam_channel::unbounded();
+
+    let mut threads = Vec::new();
+    for _ in 0..num_threads {
+       let receiver = in_receiver.clone();
+       let sender = out_sender.clone();
+       threads.push(thread::spawn(move || {
+            while let Ok(msg) = receiver.recv() {
+                let (i, next_num) = msg;
+                sender.send((i, f(next_num))).unwrap();
+            }
+       }));
+    }
+
+    drop(out_sender);
+    drop(in_receiver);
+
+    for (i, input) in input_vec.into_iter().enumerate() {
+        in_sender.send((i, input)).expect("Failed to send item to worker threads");
+    }
+
+    // break the loop in worker threads
+    drop(in_sender);
+
+    // the loop will end when all out senders are dropped and the channel is closed
+    while let Ok(out_msg) = out_receiver.recv() {
+        let (i, out_item) = out_msg;
+        output_vec[i] = out_item;
+    }
+
     output_vec
 }
 
